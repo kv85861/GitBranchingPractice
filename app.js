00b61e73ce56196855a6297ea3c024c56af8c203
vidyullatha <46826728+kv85861@users.noreply.github.com>
2019-02-28 17:17:20 -0800

    added sumarrya

--- app.js
+++ app.js
@@ -24,4 +24,18 @@ function sum(a,b) { //eslint-disable-line
         //console.log(sumarr[1]);
         return mularr;
         }
-        
+        function sumArray( test) { //eslint-disable-line
+            var sumarr =0;
+            var summarrelements=[];
+            for (var i = 0; i < test.length; i++) {
+            sumarr+=test[i];
+            //sumarr.push(test[i]);
+            // console.log(sumarr);
+            }
+            summarrelements.push(sumarr);
+            var data= test[0] + ','+test[1]+ ','+ test[2]+ ' was passed in as an array of numbers, and ' + sumarr + ' is their sum.';
+            summarrelements.push(data);
+            //console.log(summarrelements);
+            return summarrelements;
+            }
+            
